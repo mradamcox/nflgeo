85baa19e3cc909e1a4a3162e77ac9ecdb1895af8
Adam Cox <mr.adamcox@gmail.com>
2017-01-12 16:46:09 -0600

    add urls to accomodate new index view, and json returns for college points
    Signed-off-by: Adam Cox <mr.adamcox@gmail.com>

--- nflmap/urls.py
+++ nflmap/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.team_list, name='team_list'),
-    url(r'^(?P<code>[\w-]+)/$', views.team_page, name='team_page'),
+    #url(r'^$', views.team_list, name='team_list'),
+    url(r'^$', views.index, name='index'),
+    url(r'^team/(?P<code>[\w-]+)/$', views.team_page, name='team_page'),
+    url(r'^json/(?P<code>[\w-]+)/$', views.get_colleges_json, name='college_json'),
+    
+    
 ]
