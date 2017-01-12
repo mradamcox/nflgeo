44eeaf2e0113c2bec2bb5c8a5d0262271f6de9e9
Adam Cox <mr.adamcox@gmail.com>
2017-01-12 16:50:05 -0600

    add view to return json for colleges
    Signed-off-by: Adam Cox <mr.adamcox@gmail.com>

--- nflmap/views.py
+++ nflmap/views.py
@@ -34,3 +34,23 @@ def team_page(request,code):
         "col_json":team.get_college_json(),
     }
     return render(request, 'team_page.html', context)
+        
+def get_colleges_json(self,code="all"):
+
+    if code == "all":
+        q1 = College.objects.all()
+        q2 = q1.order_by('nfl_name')
+
+    else:
+        upcode = code.upper()
+        team = Team.objects.get(code=upcode)
+        players = team.get_players()
+        p_ids = [p.college.pk for p in players]
+        q1 = College.objects.filter(pk__in=p_ids)
+        q2 = q1.order_by('nfl_name')
+    
+    json = serialize('geojson', q2,geometry_field="geom",
+          fields=('nfl_name',))
+    
+    #return JsonResponse(json, safe=False)
+    return HttpResponse(json, content_type='application/json')
